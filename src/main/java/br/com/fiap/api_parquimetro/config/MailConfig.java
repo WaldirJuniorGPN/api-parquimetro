82eb6e7e498a52c54318c3e1a2e2833c7ce283c4
Waldir Júnior <waldirjunior.gpn@gmail.com>
2024-07-11 14:05:41 -0300

    refactor: Utilize variáveis de ambiente para configuração de email
    - Removidas credenciais sensíveis do código-fonte
    - Adicionadas variáveis de ambiente para EMAIL_USER e EMAIL_PASSWORD
    - Atualizada a classe MailConfig para usar as variáveis de ambiente

--- src/main/java/br/com/fiap/api_parquimetro/config/MailConfig.java
+++ src/main/java/br/com/fiap/api_parquimetro/config/MailConfig.java
@@ -1,5 +1,6 @@
 package br.com.fiap.api_parquimetro.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,14 +8,21 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
+
+    @Value("${EMAIL_USER}")
+    private String emailUser;
+
+    @Value("${EMAIL_PASSWORD}")
+    private String emailPassword;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         var mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("postechchallenge@gmail.com");
-        mailSender.setPassword("Parquimetro@@");
+        mailSender.setUsername(emailUser);
+        mailSender.setPassword(emailPassword);
 
         var props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
