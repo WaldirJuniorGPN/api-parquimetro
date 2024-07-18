0346d4c0757f8258daa0aaa3dfd72309b661d58f
Henrique Geenen <henriqueggo@hotmail.com>
2024-07-18 02:50:16 -0300

    feature-docker-image: changes for deploying on aws ecs

--- src/main/java/br/com/fiap/api_parquimetro/ApiParquimetroApplication.java
+++ src/main/java/br/com/fiap/api_parquimetro/ApiParquimetroApplication.java
@@ -3,7 +3,6 @@ package br.com.fiap.api_parquimetro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
