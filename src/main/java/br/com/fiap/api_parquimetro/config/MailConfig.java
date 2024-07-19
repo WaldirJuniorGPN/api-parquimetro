99a556fbe971693cabe8786f3511806637993798
Henrique Geenen <henriqueggo@hotmail.com>
2024-07-18 22:01:00 -0300

    Changes on application properties

---
+++ src/main/java/br/com/fiap/api_parquimetro/config/MailConfig.java
@@ -0,0 +1,35 @@
+package br.com.fiap.api_parquimetro.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.username}")
+    private String emailUser;
+
+    @Value("${spring.mail.password}")
+    private String emailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        var mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(emailUser);
+        mailSender.setPassword(emailPassword);
+
+        var props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
